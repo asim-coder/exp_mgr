507757dc7270ce687fa2508695bc32a97f190df0
Asim K T <asim@alraislabs.com>
2014-09-29 16:55:15 +0530

    Working server communication- json data recieved.

---
+++ js/downloadpdffile.js
@@ -0,0 +1,24 @@
+
+function downloadPDFFile() {
+    var fileDownloadPDF = new FileTransfer();
+    var uri = encodeURI('http://alrais.host22.com/agreement.pdf');
+    var folder = '/sdcard/';
+    var fileName = 'agreement.pdf';
+    var pathToFile = folder + fileName;
+    console.log("Hai")
+    fileDownloadPDF.download(
+                      uri,
+                      pathToFile,
+                      function(entry) {
+                      console.log("download complete: " + entry.pathToFile);
+                      alert("File Downloaded. Saved in your phone.");
+                      window.plugins.pdfViewer.showPdf(pathToFile);
+                      },
+                      function(error) {
+                      console.log("download error source " + error.source);
+                      console.log("download error target " + error.target);
+                      console.log("upload error code" + error.code);
+                      alert("Download error!!.Please make sure you have working internet connection!")
+                      }
+                      );
+}
